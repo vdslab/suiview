4db1f251c0c40923202b7659ea593308e3a23d0a
Tsubame <watamisa08130505@outlook.jp>
2020-04-20 02:13:34 +0900

    wavで保存できるようにした(バグあり)

--- client/src/pages/root/index.js
+++ client/src/pages/root/index.js
@@ -9,6 +9,11 @@ import {
   IonAlert,
 } from "@ionic/react";
 
+/*--------webmの場合-------------------------
+//録音した日付の入力
+//コメント(日付も)
+//録音した物の再生
+
 let recorder = null;
 
 const musicRecord = () => {
@@ -23,6 +28,8 @@ const musicRecord = () => {
   function successFunc(stream) {
     recorder = new MediaRecorder(stream, {
       mimeType: "video/webm;codecs=vp9", //ここは何にするのがベスト?
+      //mimeType: "audio/wav;MPEG-4 AVC",
+      //mimeType: "video/avi;codecs=dv",
     });
 
     //録音
@@ -62,8 +69,145 @@ const finPushed = () => {
   recorder.stop();
   console.log("button stop");
 };
+*/
 
+//wavの場合-------------------------------------------------
+// for html
+//const stopButton = document.getElementById("stop");
 
+// for audio
+let audio_sample_rate = null;
+let audioContext = null;
+// audio data
+let audioData = [];
+
+const musicRecord = () => {
+  const bufferSize = 1024;
+  let scriptProcessor = null;
+  const handleSuccess = (stream) => {
+    audioContext = new AudioContext();
+    audio_sample_rate = audioContext.sampleRate;
+    console.log(audio_sample_rate);
+    scriptProcessor = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    const mediastreamsource = audioContext.createMediaStreamSource(stream);
+    mediastreamsource.connect(scriptProcessor);
+    scriptProcessor.onaudioprocess = onAudioProcess;
+    scriptProcessor.connect(audioContext.destination);
+
+    console.log("record start?");
+
+    /*when time passed without pushing the stop button
+    setTimeout(function () {
+      console.log("10 sec");
+      if (stopButton.disabled == false) {
+        saveAudio();
+        console.log("saved audio");
+      }
+    }, 5000);*/
+  };
+
+  //save audio data
+  const onAudioProcess = (e) => {
+    const input = e.inputBuffer.getChannelData(0);
+    const bufferData = new Float32Array(bufferSize);
+    for (let i = 0; i < bufferSize; i++) {
+      bufferData[i] = input[i];
+    }
+    audioData.push(bufferData);
+  };
+
+  // getUserMedia
+  navigator.mediaDevices
+    .getUserMedia({ audio: true, video: false })
+    .then(handleSuccess);
+};
+
+const saveAudio = () => {
+  // export WAV from audio float data
+  const exportWAV = (audioData) => {
+    const encodeWAV = (samples, sampleRate) => {
+      const buffer = new ArrayBuffer(44 + samples.length * 2);
+      const view = new DataView(buffer);
+
+      const writeString = (view, offset, string) => {
+        for (let i = 0; i < string.length; i++) {
+          view.setUint8(offset + i, string.charCodeAt(i));
+        }
+      };
+
+      const floatTo16BitPCM = (output, offset, input) => {
+        for (let i = 0; i < input.length; i++, offset += 2) {
+          let s = Math.max(-1, Math.min(1, input[i]));
+          output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+        }
+      };
+
+      writeString(view, 0, "RIFF"); // RIFFヘッダ
+      view.setUint32(4, 32 + samples.length * 2, true); // これ以降のファイルサイズ
+      writeString(view, 8, "WAVE"); // WAVEヘッダ
+      writeString(view, 12, "fmt "); // fmtチャンク
+      view.setUint32(16, 16, true); // fmtチャンクのバイト数
+      view.setUint16(20, 1, true); // フォーマットID
+      view.setUint16(22, 1, true); // チャンネル数
+      view.setUint32(24, sampleRate, true); // サンプリングレート
+      view.setUint32(28, sampleRate * 2, true); // データ速度
+      view.setUint16(32, 2, true); // ブロックサイズ
+      view.setUint16(34, 16, true); // サンプルあたりのビット数
+      writeString(view, 36, "data"); // dataチャンク
+      view.setUint32(40, samples.length * 2, true); // 波形データのバイト数
+      floatTo16BitPCM(view, 44, samples); // 波形データ
+
+      return view;
+    };
+
+    const mergeBuffers = (audioData) => {
+      let sampleLength = 0;
+      for (let i = 0; i < audioData.length; i++) {
+        sampleLength += audioData[i].length;
+      }
+      const samples = new Float32Array(sampleLength);
+      let sampleIdx = 0;
+      for (let i = 0; i < audioData.length; i++) {
+        for (let j = 0; j < audioData[i].length; j++) {
+          samples[sampleIdx] = audioData[i][j];
+          sampleIdx++;
+        }
+      }
+      return samples;
+    };
+
+    const dataview = encodeWAV(mergeBuffers(audioData), audio_sample_rate);
+    const audioBlob = new Blob([dataview], { type: "audio/wav" });
+    console.log(dataview);
+
+    const myURL = window.URL || window.webkitURL;
+    const url = myURL.createObjectURL(audioBlob);
+    return url;
+  };
+
+  const dl = document.querySelector("#dl");
+  dl.href = exportWAV(audioData);
+  dl.download = "test.wav";
+  alert("音声のダウンロードが可能です");
+
+  /*必要そう↓多分これがないと二回目以降がおかしくなりそう
+  audioContext.close().then(function () {
+    stopButton.setAttribute("disabled", "disabled");
+  });*/
+
+  audioData = [];
+  audio_sample_rate = null;
+  audioContext = null;
+};
+
+// stop button
+/*
+stopButton.addEventListener(function () {
+  saveAudio();
+  console.log("saved wav");
+});*/
+
+/////////////////////////////////////////////////////
 const Root = () => {
   const [showAlert, setShowAlert] = useState(false);
   return (
@@ -91,7 +235,8 @@ const Root = () => {
             {
               text: "終了",
               handler: () => {
-                finPushed();
+                // finPushed();
+                saveAudio();
               },
             },
           ]}
