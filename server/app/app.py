f5c97a507d4c8698e6cbe5c4d17b84e65ad70f30
Tsubame <watamisa08130505@outlook.jp>
2020-05-04 05:08:58 +0900

    return js data

--- server/app/app.py
+++ server/app/app.py
@@ -8,7 +8,7 @@ import numpy as np
 import scipy.io.wavfile
 import wave
 from pylab import frombuffer
-
+import requests
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -25,6 +25,7 @@ def get_musics(user_id):
 @app.route('/<user_id>/musics', methods=['PUT'])
 def put_music(user_id):
     music = Music(user_id=user_id, content=request.data)
+    print(music)
     session.add(music)
     session.commit()
     return 'received'
@@ -40,27 +41,56 @@ def get_music_content(user_id, music_id):
 
 
 #　波形
+@app.route('/<user_id>/musics/<music_id>/amplitude', methods=['GET'])
 def amplitude(user_id, music_id):
-    wav_filename = '/<user_id>/musics/<music_id>/content'  # 引数とこれはあってるのか
+    """
+    # wav_filename = '/<user_id>/musics/<music_id>/content'  # 引数とこれはあってるのか
+    music = session.query(Music).get(music_id)
+    wav_filename = music  # raw?
+    print(wav_filename)
     wf = wave.open(wav_filename, "r")
     buffer = wf.readframes(wf.getnframes())
     # 縦:振幅 2バイトずつ整理(-32768から32767)
     data = frombuffer(buffer, dtype="int16")
     # 横:サンプル数or時間
     y = len(data)  # サンプル数
-    time = float(wf.getnframes()) / wf.getframerate()
     # 時間/サンプル数で1サンプル当たりの秒数→データの範囲絞ったとき時間表示するのに使う？
-    return data, y, time
+    time = float(wf.getnframes()) / wf.getframerate()
+    """
+    # 仮
+    data = [48000, 36000, 12342, 34213, 13413]
+    y = 5
+    Datas = []
+    for i in range(y):
+        dic = {
+            "x": data[i],
+            "y":  i+1
+        }
+        Datas.append(dic)
+
+    return jsonify(Datas)
 
 
 # フーリエ変換
+@app.route('/<user_id>/musics/<music_id>/fourier', methods=['GET'])
 def fourier(user_id, music_id):
+    """
     wav_filename = '/<user_id>/musics/<music_id>/content'
     rate, data = scipy.io.wavfile.read(wav_filename)
     data = data/32768  # 振幅の配列らしい
     fft_data = np.abs(np.fft.fft(data))  # 縦:dataを高速フーリエ変換
     freList = np.fft.fftfreq(data.shape[0], d=1.0/rate)  # 横:周波数の取得
-    return fft_data, freList
+    """
+    fft_data = [3, 5, 10, 100]
+    freList = [400, 450, 500, 550]
+    Datas = []
+    for i in range(len(fft_data)):
+        dic = {
+            "x": freList[i],
+            "y": fft_data[i]
+        }
+        Datas.append(dic)
+    return jsonify(Datas)
 
 
 # スペクトログラム
