77c136de3a5adad2432995cb1e852eac84c56d6a
Tsubame <watamisa08130505@outlook.jp>
2020-05-06 04:41:45 +0900

    make data of amplitude and fourier

--- server/app/app.py
+++ server/app/app.py
@@ -3,12 +3,15 @@ from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
 from db import create_session
 from models import Music, User
-# 追加
 import numpy as np
 import scipy.io.wavfile
 import wave
 from pylab import frombuffer
 import requests
+import io
+from scipy import signal
+import matplotlib
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -36,7 +39,7 @@ def get_music_content(user_id, music_id):
     music = session.query(Music).get(music_id)
     print(type(music))
     response.data = music.content
-    #response.mimetype = "application/octet-stream"
+    # response.mimetype = "application/octet-stream"
     response.mimetype = "audio/wav"
     return response
 
@@ -44,27 +47,20 @@ def get_music_content(user_id, music_id):
 #　波形
 @app.route('/<user_id>/musics/<music_id>/amplitude', methods=['GET'])
 def amplitude(user_id, music_id):
-    """
-    # wav_filename = '/<user_id>/musics/<music_id>/content'  # 引数とこれはあってるのか
-    wf = wave.open(wav_filename, "r")
+    music = session.query(Music).get(music_id)
+    wf = wave.open(io.BytesIO(music.content))
     buffer = wf.readframes(wf.getnframes())
     # 縦:振幅 2バイトずつ整理(-32768から32767)
     data = frombuffer(buffer, dtype="int16")
     # 横:サンプル数or時間
-    y = len(data)  # サンプル数
+    x = len(data)  # サンプル数
     # 時間/サンプル数で1サンプル当たりの秒数→データの範囲絞ったとき時間表示するのに使う？
-    time = float(wf.getnframes()) / wf.getframerate()
-    """
-    wav_filename = get_music_content(user_id, music_id)
-    wf = wave.open(wav_filename, "r")
-    # 仮
-    data = [48000, 36000, 12342, 34213, 13413]
-    y = 5
+    # time = float(wf.getnframes()) / wf.getframerate()
     Datas = []
-    for i in range(y):
+    for i in range(x):
         dic = {
-            "x": data[i],
-            "y":  i+1
+            "x": i+1,
+            "y": int(data[i])
         }
         Datas.append(dic)
 
@@ -74,33 +70,37 @@ def amplitude(user_id, music_id):
 # フーリエ変換
 @app.route('/<user_id>/musics/<music_id>/fourier', methods=['GET'])
 def fourier(user_id, music_id):
-    """
-    wav_filename = '/<user_id>/musics/<music_id>/content'
-    rate, data = scipy.io.wavfile.read(wav_filename)
+    music = session.query(Music).get(music_id)
+    rate, data = scipy.io.wavfile.read(io.BytesIO(music.content))
     data = data/32768  # 振幅の配列らしい
     fft_data = np.abs(np.fft.fft(data))  # 縦:dataを高速フーリエ変換
     freList = np.fft.fftfreq(data.shape[0], d=1.0/rate)  # 横:周波数の取得
-    """
-    fft_data = [3, 5, 10, 100]
-    freList = [400, 450, 500, 550]
     Datas = []
     for i in range(len(fft_data)):
-        dic = {
-            "x": freList[i],
-            "y": fft_data[i]
-        }
-        Datas.append(dic)
+        if 0 < freList[i] and freList[i] < 8000:  # 周波数の範囲
+            dic = {
+                "x": freList[i],
+                "y": fft_data[i]
+            }
+            Datas.append(dic)
+
     return jsonify(Datas)
 
 
 # スペクトログラム
+@app.route('/<user_id>/musics/<music_id>/spectrogram', methods=['GET'])
 def spectrogram(user_id, music_id):
-    # wav_filename = ""
-    """
-     時間, 周波数, 色？= 振幅(音量)?が必要
-     色はデシベルの相対から付けてるらしい
-     heatMapに渡すなら縦横色に整理して渡すっぽい
-    """
+    music = session.query(Music).get(music_id)
+    rate, data = scipy.io.wavfile.read(io.BytesIO(music.content))
+    f, time, Sxx = signal.stft(data, rate)
+    Sxx = np.log(np.abs(Sxx))
+    # Sxxを0から1に正規化
+    norm = matplotlib.colors.Normalize(vmin=-1, vmax=1)
+    cm = matplotlib.pyplot.get_cmap("jet")
+    colors = cm(norm(Sxx))  # RGB
+    Datas = []
+
+    # return jsonify(Datas)
 
 
 if __name__ == "__main__":
