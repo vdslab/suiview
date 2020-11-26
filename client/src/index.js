4ad5246aa531ea78897600f00a35c70de2e2de54
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2020-11-26 23:34:26 +0900

    push env

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 render(
   <Auth0Provider
     domain="auth0-react-test.us.auth0.com"
-    clientId="bz5xhba5hV2arCSNnSlulrypuPHgTRyd"
+    clientId={process.env.REACT_APP_CLIENT_ID}
     redirectUri={window.location.origin}
     audience="https://musicvis"
   >
