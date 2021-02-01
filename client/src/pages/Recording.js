50ecd77953781cee4857de7d9e77e589c8015912
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-02-01 14:56:55 +0900

    set recording button(just button not function)

--- client/src/pages/Recording.js
+++ client/src/pages/Recording.js
@@ -27,7 +27,13 @@ import {
 import { closeOutline, radioButtonOnOutline } from "ionicons/icons";
 import { useAuth0 } from "@auth0/auth0-react";
 import { musicRecord, saveAudio } from "../services/recording";
-import { getFolder, postMusic, putMusicContent } from "../services/api";
+import {
+  getFolder,
+  postMusic,
+  putMusicContent,
+  deleteMusic,
+} from "../services/api";
+import { Player } from "../components/Player.js";
 
 const Recording = ({ history }) => {
   const location = useLocation();
@@ -38,7 +44,9 @@ const Recording = ({ history }) => {
   const [folder, setFolder] = useState(null);
   const [showAlert, setShowAlert] = useState(false);
   const { getAccessTokenSilently } = useAuth0();
-  const [selected, setSelected] = useState();
+  const [selected, setSelected] = useState(3);
+  const [recorded, setRecorded] = useState(0);
+  const [musicId, setMusicId] = useState();
 
   useIonViewWillEnter(async () => {
     if (folderId) {
@@ -73,6 +81,7 @@ const Recording = ({ history }) => {
           <IonItem>
             <IonLabel>自己評価</IonLabel>
             <IonSelect
+              value={selected}
               onIonChange={(e) => {
                 setSelected(e.target.value);
               }}
@@ -96,15 +105,19 @@ const Recording = ({ history }) => {
       </IonContent>
       <IonFooter>
         <IonToolbar>
-          <IonButton
-            expand="full"
-            onClick={() => {
-              musicRecord();
-              setShowAlert(true);
-            }}
-          >
-            <IonIcon icon={radioButtonOnOutline}></IonIcon>
-          </IonButton>
+          {recorded == 0 ? (
+            <IonButton
+              expand="full"
+              onClick={() => {
+                //musicRecord();
+                setShowAlert(true);
+              }}
+            >
+              <IonIcon icon={radioButtonOnOutline}></IonIcon>
+            </IonButton>
+          ) : (
+            <Player musicId={musicId} />
+          )}
           <IonAlert
             isOpen={showAlert}
             onDidDismiss={() => setShowAlert(false)}
@@ -115,7 +128,7 @@ const Recording = ({ history }) => {
               {
                 text: "完了!",
                 handler: async () => {
-                  const blob = saveAudio();
+                  //const blob = saveAudio();
                   const record = { name };
                   if (folderId) {
                     record.folderId = folderId;
@@ -123,10 +136,13 @@ const Recording = ({ history }) => {
                   if (comment !== "") {
                     record.comment = comment;
                   }
-                  const music = await postMusic(blob, getAccessTokenSilently);
+                  //const music = await postMusic(blob, getAccessTokenSilently);
+                  const music = { id: 15 };
+                  setRecorded(1);
+                  setMusicId(music.id);
                   console.log(music, music.id);
                   console.log(selected);
-                  await putMusicContent(
+                  /*await putMusicContent(
                     music.id,
                     record,
                     getAccessTokenSilently
@@ -144,10 +160,33 @@ const Recording = ({ history }) => {
           <IonGrid>
             <IonRow>
               <IonCol>
-                <IonButton expand="full">やり直す</IonButton>
+                <IonButton
+                  expand="full"
+                  onClick={async () => {
+                    //await deleteMusic(musicId, getAccessTokenSilently);
+                    setRecorded(0);
+                  }}
+                >
+                  やり直す
+                </IonButton>
               </IonCol>
               <IonCol>
-                <IonButton expand="full" disabled="true">
+                <IonButton
+                  expand="full"
+                  disabled={(recorded + 1) % 2}
+                  onClick={async () => {
+                    /*await putMusicContent(
+                      music.id,
+                      record,
+                      getAccessTokenSilently
+                    );
+                    /*if (folderId) {
+                      history.replace(`/folder/${folderId}`);
+                    } else {
+                      history.replace("/musics");
+                    }*/
+                  }}
+                >
                   記録する
                 </IonButton>
               </IonCol>
