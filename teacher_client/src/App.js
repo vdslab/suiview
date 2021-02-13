77d2ed2de95d84234f798f57169a19a9b57058d5
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-02-13 19:00:34 +0900

    add change user name and refactoring

--- teacher_client/src/App.js
+++ teacher_client/src/App.js
@@ -4,13 +4,16 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useState, useEffect } from "react";
 import { putUsername, getUsername } from "./services/api/";
 
-function Header(data) {
-  const { logout, loginWithRedirect } = useAuth0();
-  const login = data.login;
-  const content = ["", "ユーザー名の変更", "ログアウト"];
-  const [selected, setSelected] = useState();
+function LoginHeader() {
+  const { loginWithRedirect } = useAuth0();
+  return <button onClick={loginWithRedirect}>login</button>;
+}
+
+function LogoutHeader() {
+  const { logout } = useAuth0();
   const { getAccessTokenSilently } = useAuth0();
   const [userData, setUserData] = useState();
+  const [reName, setRename] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -19,6 +22,53 @@ function Header(data) {
     })();
   }, [getAccessTokenSilently]);
 
+  async function sendNewName() {
+    const name = document.getElementById("name").value;
+    putUsername({ name }, getAccessTokenSilently);
+    setRename(false);
+    const data = await getUsername(getAccessTokenSilently);
+    setUserData(data);
+  }
+
+  return (
+    <div className="columns">
+      {reName === false ? (
+        <div>
+          <div className="column is-6">{userData?.name}</div>
+          <button onClick={() => setRename(true)}>ユーザー名を変更する</button>
+        </div>
+      ) : (
+        <div className="column is-6">
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            minlength="4"
+            maxlength="8"
+            size="10"
+          />
+          <button
+            onClick={() => {
+              sendNewName();
+            }}
+          >
+            これでOK！
+          </button>
+        </div>
+      )}
+      <div className="column is-6">
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+          Log out
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Header(data) {
+  const login = data.login;
+
   return (
     <section className="hero  is-small has-background-primary">
       <div className="hero-body">
@@ -26,33 +76,11 @@ function Header(data) {
           <div className="columns">
             <h1 className="title column is-9">musicvis</h1>
             <div className="column is-3">
-              {login ? (
-                <div className="columns">
-                  <div className="column is-6">{userData?.name}</div>
-                  <div className="column is-6">
-                    <button
-                      slot="end"
-                      expand="full"
-                      color="light"
-                      onClick={() =>
-                        logout({ returnTo: window.location.origin })
-                      }
-                    >
-                      Log out
-                    </button>
-                  </div>
-                </div>
-              ) : (
-                <button onClick={loginWithRedirect}>login</button>
-              )}
+              {login ? <LogoutHeader /> : <LoginHeader />}
             </div>
           </div>
         </div>
       </div>
-
-      {selected === content[2]
-        ? logout({ returnTo: window.location.origin })
-        : []}
     </section>
   );
 }
