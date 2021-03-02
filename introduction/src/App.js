8f1f7919d27f80d35e66fbd287e3d6882a5d3ed3
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-03-03 05:35:57 +0900

    add comment

--- introduction/src/App.js
+++ introduction/src/App.js
@@ -17,9 +17,16 @@ function App() {
         <section className="section">
           <div className="has-text-centered">
             <div className="title is-5">吹viewとは</div>
-            <p>ooooooooooooooooxxxxxxxxxxxxxxxxxxxxxxxxxxxoxoxoxoxoxoxo</p>
-            <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
-            <p>oxoxoxoxoxoxoxoxoxoox</p>{" "}
+            <p>
+              管楽器の練習を始めたばかりの初心者が安定した音を出せるようにするためのお手伝いをするアプリです。
+            </p>
+            <p>
+              管楽器を練習したいユーザが気軽に録音
+              し,音の強さ・高さ・音色の安定度を視覚的に確かめることができます。
+            </p>
+            <p>
+              また、先生モードからは登録したユーザの演奏データを見て、コメントをつけることができ、他者からの評価をもらうこともできます。
+            </p>{" "}
           </div>
         </section>
 
@@ -29,8 +36,12 @@ function App() {
               <img src={img} alt="イメージ画像" className="images" />
             </a>
             <div className="title is-6">生徒モード</div>
-            <p>xxxxxxxxxxxxxxxxxxxxxx</p>
-            <p>oxoxoxoxoxoxoxoxoxo</p>
+            <p>
+              ユーザーが演奏を録音すると、高さ・強さ・音色の3観点からどの程度演奏が安定しているかを点数とグラフから知ることができます。
+            </p>
+            <p>
+              また、演奏データを蓄積していくことで安定度の推移がわかります。
+            </p>
           </div>
 
           <div className="column has-text-centered section">
@@ -38,8 +49,10 @@ function App() {
               <img src={img} alt="イメージ画像" className="images" />
             </a>
             <div className="title is-6">先生モード</div>
-            <p>xxxxxxxxxxxxxxxxxxxxxx</p>
-            <p>oxoxoxoxoxoxoxoxoxo</p>
+            <p>
+              先生モードでは生徒が演奏した音源とグラフを見てコメントを付けることができます。
+            </p>
+            <p>付けたコメントは生徒が見ることができます。</p>
           </div>
         </div>
       </section>
