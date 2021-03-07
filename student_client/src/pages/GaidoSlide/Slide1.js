1277bcf4f5c8d95b6644b1a386db6d5960cb1212
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-03-07 22:14:08 +0900

    fix

--- student_client/src/pages/GaidoSlide/Slide1.js
+++ student_client/src/pages/GaidoSlide/Slide1.js
@@ -1,14 +1,14 @@
 import { IonSlide } from "@ionic/react";
-//import iconImg from "../../images/icon.PNG";
+import iconImg from "../../images/icon.PNG";
 const Slide1 = () => {
   return (
     <IonSlide>
-      {/*<img
+      <img
         src={iconImg}
         alt="ロゴ画像"
         className="logo"
         style={{ height: "40%" }}
-      />*/}
+      />
       <div style={{ marginTop: "2rem" }}>
         <h2>吹viewにようこそ！</h2>
         <p>このアプリでは自分の音を録音し、</p>
