5bb4b9bf8db82c226a5b7a18e7239cec7c40ac7f
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-03-10 03:21:12 +0900

    update login

--- teacher_client/src/App.js
+++ teacher_client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import { useAuth0 } from "@auth0/auth0-react";
+import img from "./images/ogp.png";
 
 function Login() {
   const { loginWithRedirect } = useAuth0();
@@ -8,34 +9,45 @@ function Login() {
     <div>
       <section className="hero  is-small has-background-primary">
         <div className="hero-body">
-          <div className="container">
-            <div className="columns">
-              <h1 className="title column is-9">吹View♪</h1>
-              <div className="column is-3"></div>
-            </div>
+          <div className="container" style={{ padding: "40px" }}>
+            <h1 className="title  has-text-centered">
+              吹View♪<span className="is-size-5">-先生モード-</span>
+            </h1>
           </div>
         </div>
       </section>
       <section className="hero">
         <div className="hero-body">
           <div className="has-text-centered">
-            <section>説明とか</section>
-            <br />
-            <button onClick={loginWithRedirect} className="button">
-              login
-            </button>
+            <section>
+              <h2 className="title is-5">吹viewとは</h2>
+              <p>
+                管楽器の練習を始めたばかりの初心者が安定した音を出せるようにするためのお手伝いをするアプリです。
+                管楽器を練習したいユーザが気軽に録音し,音の強さ・高さ・音色の安定度を視覚的に確かめることができます。
+              </p>
+              <br />
+              <h2 className="title is-5">先生モードでできること</h2>
+              <p>
+                先生モードでは生徒が演奏した音源とグラフを見てコメントを付けることができます。
+                付けたコメントは生徒が見ることができます。
+              </p>
+            </section>
+            <section className="section">
+              <button
+                onClick={loginWithRedirect}
+                className="button is-primary is-outlined"
+              >
+                login
+              </button>
+            </section>
+            <section>
+              <h2 className="title is-6">生徒モードはこちら↓</h2>
+              <img src={img} alt="先生モード" style={{ width: "40vmin" }} />
+            </section>
           </div>
         </div>
       </section>
     </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer className="footer">
-      <div className="content has-text-centered">(_ _)....</div>
-    </footer>
   );
 }
 
@@ -65,7 +77,6 @@ export function App() {
       ) : (
         <Login />
       )}{" "}
-      <Footer />
     </Router>
   );
 }
