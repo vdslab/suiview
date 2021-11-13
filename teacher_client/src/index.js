e57f37cc96cd011caf75844bcb170b0fae10436d
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2021-11-14 00:19:47 +0900

    translationの導入

--- teacher_client/src/index.js
+++ teacher_client/src/index.js
@@ -2,6 +2,17 @@ import "bulma/css/bulma.css";
 import { render } from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import resources from "./translation.js";
+
+i18n.use(LanguageDetector).use(initReactI18next).init({
+  resources,
+  lang: "ja",
+  fallbackLng: "en",
+});
 
 render(
   <Auth0Provider
--- teacher_client/src/pages/Home.js
+++ teacher_client/src/pages/Home.js
@@ -4,11 +4,13 @@ import FolderList from "../components/FolderList";
 import StudentList from "../components/StudentList";
 import Folder from "../components/Folder";
 import { putUsername, getUsername } from "../services/api/";
+import { useTranslation } from "react-i18next";
 
 function Account() {
   const [userData, setUserData] = useState();
   const [reName, setRename] = useState(false);
   const { getAccessTokenSilently } = useAuth0();
+  const { t, i18n } = useTranslation();
 
   useEffect(() => {
     (async () => {
@@ -44,9 +46,9 @@ function Account() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="#000000"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinejoin="round"
               >
                 <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
                 <circle cx="12" cy="10" r="3" />
@@ -87,6 +89,20 @@ function Account() {
                 </div>
               )}
             </div>
+          </div>
+        </div>
+        <div className="pt-6">
+          <div className="title is-5">{t("changeLanguage")}</div>
+          <div className="select is-small is-primary">
+            <select
+              value={i18n.language.slice(0, 2)}
+              onChange={(event) => {
+                i18n.changeLanguage(event.currentTarget.value);
+              }}
+            >
+              <option value="ja">{t("japanese")}</option>
+              <option value="en">{t("english")}</option>
+            </select>
           </div>
         </div>
       </section>
