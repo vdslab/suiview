8f35aeeae18454b41f23db33e564abade40f8141
Yosuke Onoue <onoue@likr-lab.com>
2021-11-16 02:30:55 +0900

    add GA

--- student_client/src/App.js
+++ student_client/src/App.js
@@ -1,7 +1,9 @@
-import { Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, useLocation } from "react-router-dom";
 import { IonApp, IonRouterOutlet } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import { useAuth0 } from "@auth0/auth0-react";
+import ReactGA from "react-ga";
 import {
   Home,
   Login,
@@ -15,27 +17,39 @@ import {
 } from "../src/pages";
 import "./theme/variables.css";
 
+ReactGA.initialize("G-YHK6XHHLTE");
+
+const Tracker = ({ children }) => {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname);
+  }, [location.pathname]);
+  return <>{children}</>;
+};
+
 const App = () => {
   const { isAuthenticated } = useAuth0();
   //console.log("visited" in localStorage);
   return (
     <IonApp>
       <IonReactRouter>
-        {isAuthenticated ? (
-          <IonRouterOutlet>
-            <Route path="/home" component={Home} exact />
-            <Route path="/" component={Home} exact />
-            <Route path="/setting/gaido" component={Guide} />
-            <Route path="/recording" component={Recording} />
-            <Route path="/setting" component={Setting} exact />
-            <Route path="/musics" component={MusicList} />
-            <Route path="/folder/:folderId" component={Folder} />
-            <Route path="/detail/:musicId" component={Detail} />
-            <Route path="/select_folder/:musicId" component={SelectFolder} />
-          </IonRouterOutlet>
-        ) : (
-          <Login />
-        )}
+        <Tracker>
+          {isAuthenticated ? (
+            <IonRouterOutlet>
+              <Route path="/home" component={Home} exact />
+              <Route path="/" component={Home} exact />
+              <Route path="/setting/gaido" component={Guide} />
+              <Route path="/recording" component={Recording} />
+              <Route path="/setting" component={Setting} exact />
+              <Route path="/musics" component={MusicList} />
+              <Route path="/folder/:folderId" component={Folder} />
+              <Route path="/detail/:musicId" component={Detail} />
+              <Route path="/select_folder/:musicId" component={SelectFolder} />
+            </IonRouterOutlet>
+          ) : (
+            <Login />
+          )}
+        </Tracker>
       </IonReactRouter>
     </IonApp>
   );
